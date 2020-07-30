c4135166a3909870ff182eb503e9c8adc011c958
rajesh <>
2020-07-30 21:41:10 +0530

    Added signin page and its styles  and custom button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 
 class App extends Component {
   render() {
@@ -14,6 +16,7 @@ class App extends Component {
         <Switch>
           <Route exact path = "/" component = {HomePage} />
           <Route path = "/shop" component = {ShopPage} />
+          <Route path = "/signin" component = {SignInAndSignUpPage} />
         </Switch>
       </div>
     ) 

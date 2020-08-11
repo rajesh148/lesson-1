2baddafb6bfba88b0a5414cc83431d3e245203a5
rajesh <>
2020-08-11 08:59:43 +0530

    implement firebase utils, incluce ability to store authenticated users into our firestore database

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,6 +4,7 @@ import {withRouter} from 'react-router-dom';
 import "./menu-item.style.scss"
 
 const MenuItem = ({title , imageUrl, size, history, linkUrl, match}) => (
+
     <div className = {`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)} >
         <div
             className = "background-image"

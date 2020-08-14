859ce2d3cd015422744226d2d7182c2b35c375ce
rajesh <>
2020-08-14 07:38:50 +0530

    Added redux, and implemented userReducer and userActions to header and app components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.style.scss';
 
@@ -15,10 +15,16 @@ class SignIn extends React.Component {
             password:'',
         }
     }
-    handleSubmit = (event) => {
+    handleSubmit = async event => {
         event.preventDefault();
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email:'', password:''});
+        } catch (error) {
+            console.log(error);
+        }
 
-        this.setState({email:'', password:''});
     }
 
     handleChange = (event) => {
